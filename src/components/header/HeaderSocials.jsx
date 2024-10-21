178f9cc835d360d08b56ab891188d1422d6ba5e0
sai satwika <saisatwikadhupam@gmail.com>
2024-10-21 16:03:02 +0530

    Update homepage for Netlify deployment

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-
+import './HeaderSocials.css';
 
 const HeaderSocials = () => {
   return (
+    
     <div className="header__socials">
       <a href="https://www.linkedin.com/in/dhupam-saisatwika-b68b52284/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
       <a href="https://github.com/Chocomimin" target="_blank" rel="noreferrer" ><FaGithub /></a>
